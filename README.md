c372b012415eca5cc646b8df0175d88eb508f180
一个队 <zhangjingmeng999@163.com>
2020-03-01 21:00:33 +0800

    0301 add basic frontend file

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 2019 School Innovation Project (YuYue)
 
 Project is divided into backend and frontend.
+
+
+
+Update basic frontend in folder frontend.
+
++ 建立个人分支，两天左右与dev分支合并一次，与dev分支合并时更新readme
+
++ 网页调试 在vue-app中，使用`npm run dev`(见vue-app readme.md)
+
+  生成APK  在vue-app中，使用`npm run build`
+
++ ElementUI 在网页模式和设备模式下效果会产生区别，在浏览器调试时务必使用设备模式观察效果
+
++ 设备模式选用 Galaxy S5
---
+++ frontend/joyRead/vue-app/src/views/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>Welcome to homepage of Joy Read!</div>
+    <el-button type='primary'>test button</el-button>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ 
+
+</style>
