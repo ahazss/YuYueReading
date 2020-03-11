4f279d409d5e5607a3605c1787274acca3360186
Cisy0021 <wsq3123@163.com>
2020-03-11 18:41:38 +0800

    Update Scan.vue

--- frontend/joyRead/vue-app/src/views/Scan.vue
+++ frontend/joyRead/vue-app/src/views/Scan.vue
@@ -5,12 +5,14 @@
       id="video"
       width="300"
       height="300"
-      style="margin-top:40%;margin-bottom:100%;"
+      style="margin-top:27%;margin-bottom:50%;"
     ></video>
+  <FooterBar></FooterBar>
   </div>
 </template>
 
 <script>
+import FooterBar from "../components/FooterBar"
 // eslint-disable-next-line no-unused-vars
 import adapter from 'webrtc-adapter';
 // WebRTC适配器 只需要引入就ok
@@ -19,6 +21,10 @@ import { BrowserMultiFormatReader } from '@zxing/library';
  * zxing demo
  */
 export default {
+  name:'Scan',
+  components: {
+        FooterBar,
+    },
   data: () => ({
     codeReader: new BrowserMultiFormatReader(),
     textContent: undefined
@@ -44,13 +50,25 @@ export default {
       });
   },
   methods: {
+    goBack() {
+        this.$router.go(-1);
+        console.log('go back');
+        },
   }
 };
 </script>
 
 <style lang="scss" scoped>
+#back{
+  position: relative;
+  float: left;
+  margin-left: 3%;
+  margin-top: 0px;
+}
 .v-body{
     background: url('../assets/img/scan-bg.png');
     background-repeat:no-repeat;
+    background-position: absolute;;
+    background-size:100% 100%;
 }
 </style>
